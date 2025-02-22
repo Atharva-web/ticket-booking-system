8fc37a8b6e0177878fbc0b0651d42a0802aed589
Atharva-web <ath3arv8a2@gmail.com>
2025-02-22 13:31:04 +0530

    deleted files

--- app/src/main/java/ticket/booking/entities/Train.java
+++
@@ -1,9 +0,0 @@
-package ticket.booking.entities;
-
-import java.util.List;
-
-public class Train {
-    private String trainId;
-    private String ticketNumber;
-    private List<List<Integer>> seats;
-}
